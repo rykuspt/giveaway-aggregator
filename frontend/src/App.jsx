8dcc35540ae5d9d62e9f7975eb08148288db793b
Folhas <gtaff2001@gmail.com>
2026-06-16 23:39:18 +0100

    app changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const API_URL = 'https://giveaway-aggregator.onrender.com';
 
 /* eslint-disable no-unused-vars */
 
